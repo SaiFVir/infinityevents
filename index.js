e497fa553adf3f29a3d02d7895830e1b857844e5
SaiFVir <43346721+SaiFVir@users.noreply.github.com>
2019-08-03 01:42:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -352,7 +352,7 @@ bot.on('message', async (message)=>{
         //http://i8.ae/aZWp
         case 'info-mob':
             if(!ms.member.roles.find(r => r.name === "events"|| ms.member.hasPermission("ADMINISTRATOR"))) return m.send("`ليس لديك الصلاحية لأستخدام هذا الأمر`").then(ms => ms.delete(5000));
-            for(i in b){if(mc.includes(b[i])){if(a[0]==b[i]){e.setDescription("**All departments are working with one perfix `in-`**\n\n`in-info-lap` : **الصفحة الحالية لللابتوب**\n`in-info-mob` : **الصفحة الحالية للموبايل**\n`-للحصول على أجوبة الأقسام المُميزة بنقطة فقط قبل القسم أكتب ج`\n`مُلاحظة : يبدأ عرض الصور من رقم 0 والرقم الأخير لِكُل قسم لا يُحسب`")
+            for(i in b){if(mc.includes(b[i])){if(a[0]==b[i]){e.setDescription("**All departments are working with one perfix `-`**\n\n`in-info-lap` : **الصفحة الحالية لللابتوب**\n`in-info-mob` : **الصفحة الحالية للموبايل**\n`-للحصول على أجوبة الأقسام المُميزة بنقطة فقط قبل القسم أكتب ج`\n`مُلاحظة : يبدأ عرض الصور من رقم 0 والرقم الأخير لِكُل قسم لا يُحسب`")
                 .setImage('http://i8.ae/aZWp').setColor('RANDOM').setFooter("Developed by SAIF");m.sendEmbed(e);}}
         }break;
         case 'ج-كلمة-وأغـ':
