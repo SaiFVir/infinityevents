39f68df400d06738d32ada1973079597c3432485
SaiFVir <43346721+SaiFVir@users.noreply.github.com>
2019-08-18 09:20:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -435,22 +435,21 @@ bot.on("message",async msg => {
 });
 
 bot.on("message",async msg => {
-	let args = msg.content.substring(perfix.length).split(".");
-	let cmd = args[0];
-	if(cmd === 'info'){
+	let args = msg.content.split(" ");
+
+	if(args[0].startsWith(perfix + 'info')){
 		if(msg.author.id !== ownerid) return;
 		let e = new Discord.RichEmbed();
-		let ms = msg.channel;
 
 		e.setAuthor(`${msg.guild.name}`, `${msg.guild.iconURL}`)
-		.addField('\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC', `(Channels/\`${msg.guild.channels.size}\`)\n(Roles/\`${msg.guild.roles.size}\`)\n(Members/\`${msg.guild.members.filter(m => !m.user.bot).size}\`)\n(Status :${msg.guild.emojis.find(f => f.name === 'online')}\`${msg.guild.members.filter(m => m.presence.status === 'online').size}\`-${msg.guild.emojis.find(f => f.name === 'offline')}\`${msg.guild.members.filter(m => m.presence.status === 'offline').size}\`-${msg.guild.emojis.find(f => f.name === 'Idle')}\`${msg.guild.members.filter(m => m.presence.status === 'idle').size}\`-${msg.guild.emojis.find(f => f.name === 'dnd')}\`${msg.guild.members.filter(m => m.presence.status === 'dnd').size}\`\n(Bots/\`${msg.guild.members.filter(m => m.user.bot).size}\`)\n(Emojis/\`${msg.guild.emojis.size}\`)\n(Owner/${msg.guild.owner})\n(Region/\`${msg.guild.region}\`)\n(CreatedOn/\`${msg.guild.createdAt.getFullYear()+'/'+msg.guild.createdAt.getDay()+'/'+msg.guild.createdAt.getHours()}\`)`)
+		.addField('\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC\u30FC', `(Channels/\`${msg.guild.channels.size}\`)\n(Roles/\`${msg.guild.roles.size}\`)\n(Members/\`${msg.guild.members.filter(m => !m.user.bot).size}\`)\n(Status :${msg.guild.emojis.find(f => f.name === 'online')}\`${msg.guild.members.filter(m => m.presence.status === 'online').size}\` -${msg.guild.emojis.find(f => f.name === 'offline')}\`${msg.guild.members.filter(m => m.presence.status === 'offline').size}\` -${msg.guild.emojis.find(f => f.name === 'Idle')}\`${msg.guild.members.filter(m => m.presence.status === 'idle').size}\` -${msg.guild.emojis.find(f => f.name === 'dnd')}\`${msg.guild.members.filter(m => m.presence.status === 'dnd').size}\`)\n(Bots/\`${msg.guild.members.filter(m => m.user.bot).size}\`)\n(Emojis/\`${msg.guild.emojis.size}\`)\n(Owner/${msg.guild.owner})\n(Region/\`${msg.guild.region}\`)\n(CreatedOn/\`${msg.guild.createdAt.getFullYear()+'/'+msg.guild.createdAt.getDay()+'/'+msg.guild.createdAt.getHours()}\`)`)
 		.setThumbnail(`${msg.guild.iconURL}`)
 		.setColor('#2a7bb5')
 		.setDescription('**A collection of information**')
 		.setFooter(`${msg.author.username}`, `${msg.author.displayAvatarURL}`)
 		.setTimestamp()
-		ms.send(e);
-	} //
+		msg.channel.send(e);
+	}
 });
 
 }catch(err){throw(err)}
